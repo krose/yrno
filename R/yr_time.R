ff7bb55c7b13f7376c7d7209a48a0fad063c9423
Kenneth Rose <kennethrose82@gmail.com>
2016-12-01 14:30:16 +0100

    yr_time now parse the values to double or integer.

--- R/yr_time.R
+++ R/yr_time.R
@@ -25,7 +25,9 @@ yr_time <- function(yr_content){
         purrr::map(~data.frame(variable = names(.), value = ., row.names = NULL, stringsAsFactors = FALSE)) %>%
         purrr::map(~tidyr::spread(., variable, value)) %>%
         dplyr::bind_rows(), silent = TRUE)
-  try(names(yr_list$yr_symbol) <- paste0("symbol_", names(yr_list$yr_symbol)))
+  try(names(yr_list$yr_symbol) <- paste0("symbol_", names(yr_list$yr_symbol)), silent = TRUE)
+  try(yr_list$yr_symbol$symbol_number <- as.integer(yr_list$yr_symbol$symbol_number), silent = TRUE)
+  try(yr_list$yr_symbol$symbol_numberex <- as.integer(yr_list$yr_symbol$symbol_numberex), silent = TRUE)
 
   try(yr_list$yr_precipitation <-
         yr_content %>%
@@ -37,6 +39,7 @@ yr_time <- function(yr_content){
         purrr::map(~tidyr::spread(., variable, value)) %>%
         dplyr::bind_rows(), silent = TRUE)
   try(names(yr_list$yr_precipitation) <- paste0("precipitation_", names(yr_list$yr_precipitation)))
+  try(yr_list$yr_precipitation$precipitation_value <- as.double(yr_list$yr_precipitation$precipitation_value), silent = TRUE)
 
 
   try(yr_list$yr_winddirection <-
@@ -49,6 +52,7 @@ yr_time <- function(yr_content){
         purrr::map(~tidyr::spread(., variable, value)) %>%
         dplyr::bind_rows(), silent = TRUE)
   try(names(yr_list$yr_winddirection) <- paste0("winddirection_", names(yr_list$yr_winddirection)))
+  try(yr_list$yr_winddirection$winddirection_deg <- as.double(yr_list$yr_winddirection$winddirection_deg), silent = TRUE)
 
   try(yr_list$yr_windspeed <-
         yr_content %>%
@@ -60,6 +64,7 @@ yr_time <- function(yr_content){
         purrr::map(~tidyr::spread(., variable, value)) %>%
         dplyr::bind_rows(), silent = TRUE)
   try(names(yr_list$yr_windspeed) <- paste0("windspeed_", names(yr_list$yr_windspeed)))
+  try(yr_list$yr_windspeed$windspeed_mps <- as.double(yr_list$yr_windspeed$windspeed_mps), silent = TRUE)
 
   try(yr_list$yr_temperature <-
         yr_content %>%
@@ -71,6 +76,7 @@ yr_time <- function(yr_content){
         purrr::map(~tidyr::spread(., variable, value)) %>%
         dplyr::bind_rows(), silent = TRUE)
   try(names(yr_list$yr_temperature) <- paste0("temperature_", names(yr_list$yr_temperature)))
+  try(yr_list$yr_temperature$temperature_value <- as.double(yr_list$yr_temperature$temperature_value), silent = TRUE)
 
   try(yr_list$yr_pressure <-
         yr_content %>%
@@ -82,6 +88,7 @@ yr_time <- function(yr_content){
         purrr::map(~tidyr::spread(., variable, value)) %>%
         dplyr::bind_rows(), silent = TRUE)
   try(names(yr_list$yr_pressure) <- paste0("pressure_", names(yr_list$yr_pressure)))
+  try(yr_list$yr_pressure$pressure_value <- as.double(yr_list$yr_pressure$pressure_value), silent = TRUE)
 
   yr_df <-
     yr_list %>%
